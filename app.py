1b35b2e3b9c2f36ee5d015468ac7c374c5fb1c8f
GyeongSuKim <rla04722@naver.com>
2022-05-16 13:56:24 +0900

    Feat : sign 정보 저장
    이메일, 패스워드 정보 로컬db에 저장

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from crypt import methods
-from flask import Flask, jsonify
-from requests import request
+import email
+import json
+from msilib.schema import ODBCAttribute
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
-
-
+cors = CORS(app, resources={r'*': {'origins': '*'}})
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 
 @app.route("/")
@@ -13,10 +16,19 @@ def hello_word():
     return jsonify({'mseege':'success'})
 
 
-@app.route("/singup", methods=["POST"])
+@app.route("/signup", methods=["POST"])
 def sign_up():
-    print(request)
+
+    data = json.loads(request.data)
+    print(data.get('email'))
+    print(data['password'])
     
+    doc = {
+        'email' : data.get('email'),
+        'password' : data.get('password')
+    }
+    
+    db.user_signup.insert_one(doc)
     return jsonify({'mseege':'success'})
 
 
